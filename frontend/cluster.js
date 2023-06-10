b6f8af3ed01fca0c81e58bd11270d0bdae776e67
nicocanta20 <nicocanta20@gmail.com>
2023-06-10 18:25:48 -0300

    cluster tooltip working

--- frontend/cluster.js
+++ frontend/cluster.js
@@ -8,30 +8,21 @@ const color = d3.scaleOrdinal()
     .domain(['gonza', 'nico', 'flori'])
     .range(['blue', 'green', 'red']);
 
-const opacidad = d3.scaleLinear().range([.1, 1]);
+const opacidad = d3.scaleLinear().range([.05, 1]);
 const radio = d3.scaleRadial().range([8, 80]);
+const strokeWidth = d3.scaleLinear().range([3, 10]); // added this line
 
-// Create a tooltip
-// var Tooltip = d3.select("#visualization")
-//     .append("div")
-//     .style("opacity", 0)
-//     .attr("class", "tooltip")
-//     .style("background-color", "white")
-//     .style("border", "solid")
-//     .style("border-width", "2px")
-//     .style("border-radius", "5px")
-//     .style("padding", "5px")
-//     .style("position", "absolute") // <-- Add this line
-//     .style("z-index", "10");
+let tooltip = d3.select("#visualization").append("div")
+  .attr("class", "tooltiptest")
+  .style("opacity", 0)
 
-let tooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
+console.log(tooltip.node());
 
-d3.csv('../data.csv', d3.autoType).then(data => {
+d3.csv('../data3.csv', d3.autoType).then(data => {
     nodos = data;
-    opacidad.domain(d3.extent(nodos, d => d.loudness));
+    opacidad.domain(d3.extent(nodos, d => d.energy));
     radio.domain(d3.extent(nodos, d => +d.danceability));
+    strokeWidth.domain(d3.extent(nodos, d => +d.valence)); // added this line
 
     const svg = d3.select('#visualization').append('svg').attr('width', width).attr('height', height);
     chart = svg
@@ -58,7 +49,8 @@ function createSimulation(nodos) {
         .force('manybody', d3.forceManyBody().strength(200))
         .force('center', d3.forceCenter())
         .force('y', d3.forceY().strength(0.1))
-        .force('collide', d3.forceCollide(d => radio(+d.danceability) + 1).strength(2).iterations(5))
+        // .force('collide', d3.forceCollide(d => radio(+d.danceability) + 1).strength(2).iterations(5))
+        .force('collide', d3.forceCollide(d => radio(+d.danceability) +2).strength(2).iterations(5)) // added stroke width here
         .on('tick', redraw);
 }
 
@@ -76,51 +68,30 @@ function draw(chart, nodos) {
         .append('circle')
         .attr('r', d => radio(+d.danceability))
         .style('fill', (d, i) => color(d.user))
-        .style('fill-opacity', d => opacidad(d.loudness)/1.1)
-        .style('stroke', d=> color(d.user)) // added stroke color
-        .style('stroke-width', '3px') // added stroke width
+        .style('fill-opacity', d => opacidad(d.energy)/1.1)
+        .style('stroke', d => color(d.user)) 
+        .style('stroke-width', d => strokeWidth(d.valence) + 'px') // updated this line
         .on("mouseover", function(event, d) {
+            d3.select(this).style('fill-opacity',  d => opacidad(d.energy)*2); // change opacity to full
             console.log(d);
-            // Tooltip
-            //     .style("opacity", 1)
-            //     .html('<u>' + d.song + '</u>' + "<br>" + "Danceability: " + d.danceability + "<br>" + "Loudness: " + d.loudness)
-            //     .style("left", (event.x+20) + "px")
-            //     .style("top", (event.y) + "px")
             tooltip.transition()
-        .duration(200)
-        .style("opacity", .9);
-      tooltip.html(d.song)
-        .style("left", (event.pageX) + "px")
-        .style("top", (event.pageY - 28) + "px");
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(d.song + '<br/>' + '<br/>' + 'Danceability:' + d.danceability + '<br/>' + 'Energy:' + d.energy )
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
         })
         .on("mousemove", function(event, d) {
             tooltip
-                .style("left", (event.x+20) + "px")
-                .style("top", (event.y) + "px")
-        })
+                .style("left", (event.pageX + 20) + "px")
+                .style("top", (event.pageY) + "px")
+        })        
         .on("mouseleave", function(d) {
-            // Tooltip
-            //     .style("opacity", 0)
+            d3.select(this).style('fill-opacity', d => opacidad(d.energy)/1.1);
             tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
+                .duration(500)
+                .style("opacity", 0);
         });
-        
-
-
-    // sessions
-    //     .append('text')
-    //     .text(d => d["song"])
-    //     .attr('y', d => radio(+d.danceability) / 20)
-    //     .attr('text-anchor', 'middle')
-    //     .attr('font-size', d => {
-    //         const size = radio(+d.danceability) / 4;
-    //         if (size > 14) {
-    //             return size + 'px';
-    //         }
-    //         return 0;
-    //     });
-
 }
 
 function redraw() {
